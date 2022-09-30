7059b45dfab3569f8d1b7205bd03d1862b532710
Rajamanikandan Soliappan <rajamanikandanso@maveric-systems.com>
2022-09-30 16:47:53 +0530

    added configuration for cross origin

--- src/main/java/com/maveric/gatewayservice/config/GatewayServiceConfig.java
+++ src/main/java/com/maveric/gatewayservice/config/GatewayServiceConfig.java
@@ -6,16 +6,38 @@ import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.Arrays;
+import java.util.Collections;
 
 @Configuration
 public class GatewayServiceConfig {
 
+//    public GatewayServiceConfig(WebClient.Builder webClientBuilder) {
+//        webClientBuilder = webClientBuilder;
+//    }
 
+    @Bean
+    public CorsWebFilter corsWebFilter() {
 
-    public GatewayServiceConfig(WebClient.Builder webClientBuilder) {
-        webClientBuilder = webClientBuilder;
+        final CorsConfiguration corsConfig = new CorsConfiguration();
+        corsConfig.setAllowedOrigins(Collections.singletonList("*"));
+        corsConfig.setMaxAge(3600L);
+        corsConfig.setAllowedMethods(Arrays.asList("GET", "POST"));
+        corsConfig.addAllowedHeader("*");
+        corsConfig.addExposedHeader("Authorization");
+
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/api/v1/**", corsConfig);
+
+        return new CorsWebFilter(source);
     }
+
 
     @Bean
     public RouteLocator gatewayRoutes(RouteLocatorBuilder builder,AuthenticationPreFilter authenticationPreFilter,
@@ -25,9 +47,8 @@ public class GatewayServiceConfig {
                 .route(routeToUser -> routeToUser.path(userProperties.getPath())
                         .filters(f ->
                                 f.filter(authenticationPreFilter.apply(
-                                                new AuthenticationPreFilter.Config())))
+                                        new AuthenticationPreFilter.Config())))
                         .uri(userProperties.getUri()))
-
                 .route(routeToBalance -> routeToBalance.path(balanceProperties.getPath())
                         .filters(f ->
                                 f.filter(authenticationPreFilter.apply(
